2ddfff70a67300d51257ce0de70f6a54c07669cf
Qezert <bekr@itu.dk>
2023-09-20 11:07:53 +0200

    Start for TCPIPSimulator

--- TCP_IP_Simulator/TheSimulator.go
+++ TCP_IP_Simulator/TheSimulator.go
@@ -1,5 +1,33 @@
 package TCP_IP_Simulator
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 /*
  1) Implement TCP/IP using threads. The implementation needs to show that we have a good understanding of the protocol.
 */
+//random number generator
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func sendSyn(ch chan int) {
+	synNum := random(0, 1000)
+	ch <- synNum
+}
+
+func sender(ch chan int) {
+
+}
+
+func receiver() {
+
+}
+
+func main() {
+
+	fmt.Println(random(0, 1000))
+
+}
