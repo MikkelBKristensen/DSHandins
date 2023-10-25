0cd0dc45865b1e07f010c0ae8ec38fbbee8cbe5f
Oliver Laursen <olau@itu.dk>
2023-10-25 10:31:24 +0200

    Client and server
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>
    Co-Authored-By: Mikkel Kristensen <mbek@itu.dk>

--- HandIn3_ChittyChat/ChittyChat_service/Server/server.go
+++ HandIn3_ChittyChat/ChittyChat_service/Server/server.go
@@ -1,1 +1,72 @@
 package Server
+
+//SRC: https://github.com/Mai-Sigurd/grpcTimeRequestExample#setting-up-the-server
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	proto "github.com/MikkelBKristensen/DSHandins/HandIn3_ChittyChat/ChittyChat_service/gRPC/chittychat.proto"
+	"google.golang.org/grpc"
+)
+
+// Struct that will be used to represent the Server.
+type Server struct {
+	proto.UnimplementedTimeAskServer // Necessary
+	name                             string
+	port                             int
+}
+
+// Used to get the user-defined port for the server from the command line
+var port = flag.Int("port", 0, "server port number")
+
+func main() {
+	// Get the port from the command line when the server is run
+	flag.Parse()
+
+	// Create a server struct
+	server := &Server{
+		name: "serverName",
+		port: *port,
+	}
+	2
+	// Start the server
+	go startServer(server)
+
+	// Keep the server running until it is manually quit
+	for {
+
+	}
+}
+
+func startServer(server *Server) {
+
+	// Create a new grpc server
+	grpcServer := grpc.NewServer()
+
+	// Make the server listen at the given port (convert int port to string)
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(server.port))
+
+	if err != nil {
+		log.Fatalf("Could not create the server %v", err)
+	}
+	log.Printf("Started server at port: %d\n", server.port)
+
+	// Register the grpc server and serve its listener
+	proto.RegisterTimeAskServer(grpcServer, server)
+	serveError := grpcServer.Serve(listener)
+	if serveError != nil {
+		log.Fatalf("Could not serve listener")
+	}
+}
+
+func (c *Server) AskForTime(ctx context.Context, in *proto.AskForTimeMessage) (*proto.TimeMessage, error) {
+	log.Printf("Client with ID %d asked for the time\n", in.ClientId)
+	return &proto.TimeMessage{
+		Time:       time.Now().String(),
+		ServerName: c.name,
+	}, nil
+}
