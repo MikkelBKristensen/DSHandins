8f3ee265b31566a1ce8beb46643fcff34e938582
olau <olau@itu.dk>
2023-11-11 15:36:37 +0100

    Commit

--- MutualExclusion/main.go
+++ MutualExclusion/main.go
@@ -2,45 +2,59 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	MeService "github.com/MikkelBKristensen/DSHandins/MutualExclusion/MeService"
 	_ "github.com/MikkelBKristensen/DSHandins/MutualExclusion/MeService"
 	"google.golang.org/grpc"
 	"log"
+	"net"
 	"os"
 )
 
 type Peer struct {
-	lamportClock      int32
+	MeService.UnimplementedMeServiceServer
 	port              string
+	server            *grpc.Server
+	client            MeService.MeServiceClient
+	lamportClock      int32
 	inCriticalSection bool
 	pendingRequests   []MeService.Request
-	peerList          []string
+	peerList          map[string]MeService.MeServiceClient
+	PortList          []string
 }
 
-func (s *Peer) sendConnectionStatus(status bool) {
+func NewPeer(port string) *Peer {
+	return &Peer{port: port}
+}
+
+func (s *Peer) sendJoin(status bool) {
 	//Status: 0 for leaving and 1 for joining
 	// Update peerList to be equal the PeerPorts file
 	// and append own port to the file
 	s.peerList, _ = readFile()
 	s.updateFile()
 
-	message := MeService.ConnectionMsg{
+	// create a new connectionMsg
+	connectionMsg := MeService.ConnectionMsg{
 		Port:   s.port,
-		isJoin: status,
+		IsJoin: status,
 	}
 
 	for i := 0; i < len(s.peerList); i++ {
-		_ = s.peerList[i]
-		//Send to all peers
+		port := s.peerList[i]
+		// TODO Connect to peer
+		// TODO Send connectionMsg to peer
 	}
 }
 
-func (s *Peer) receiveJoin(join *MeService.ConnectionMsg) {
+func (s *Peer) receiveConnectionStatus(message *MeService.ConnectionMsg) {
 	// TODO Update clock
-	if join.IsJoin == true {
-		append(s.peerList, join.GetPort())
+	if message.IsJoin == true {
+		s.connect(message.GetPort())
+		append(s.PortList, message.GetPort())
+
 	} else {
-		//Make it delete the port from the list
+		//Make it delete the port from the PeerList
 
 	}
 
@@ -80,7 +94,7 @@ func readFile() ([]string, error) {
 }
 
 func (s *Peer) updateFile() {
-	// Set filename
+	// Choose filename
 	fileName := "PeerPorts.txt"
 
 	// Open file
@@ -94,20 +108,63 @@ func (s *Peer) updateFile() {
 	if _, err := peerPortFile.WriteString(s.port + "\n"); err != nil {
 		log.Fatalf("Could not add Port: %s to file", s.port)
 	}
+
 }
 
 func (s *Peer) deleteFromFile() {
 	panic("Not implemented yet")
 }
 
-func (s *Peer) connect(port string) grpc.ClientConn {
-	panic("Not Implemented yet")
+func (s *Peer) updatePortList() (err error) {
+	s.PortList, err = readFile()
+	if err != nil {
+		log.Fatalf("Could not read from file: %s", err)
+	}
+	s.updateFile()
+
+	return nil
 }
 
-func (s *Peer) sendMessage(msg MeService.ConnectionMsg) {
-	panic("Not implemented yet")
+func (s *Peer) connect(port string) (err error) {
+	//Connect to peers
+	conn, err := grpc.Dial(":"+port, grpc.WithInsecure())
+	if err != nil {
+		return fmt.Errorf("could not connect to peer on port %s: %v", port, err)
+	}
+	// add connection to peerList
+	s.peerList[port] = MeService.NewMeServiceClient(conn)
+
+	return nil
+}
+
+func (s *Peer) StartServer() error {
+	// Create listener
+	listener, err := net.Listen("tcp", ":"+s.port)
+	if err != nil {
+		log.Fatalf("Could not listen to port: %s: %v", s.port, err)
+	}
+
+	fmt.Println("Now listening on port: ", s.port)
+
+	// Create new server
+	s.server = grpc.NewServer()
+	MeService.RegisterMeServiceServer(s.server, s)
+
+	// Start server
+	go func() {
+		if err := s.server.Serve(listener); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	s.updatePortList()
+	s.updateFile()
+
+	return nil
 }
 
 func main() {
+	Peer1 := NewPeer("5001")
+	Peer1.StartServer()
 
 }
