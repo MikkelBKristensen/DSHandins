71bf01369c71b285d18a9c5580c9e714c57e5314
olau <olau@itu.dk>
2023-11-19 10:37:30 +0100

    commit

--- Auction/Client.go
+++ Auction/Client.go
@@ -1,2 +1,11 @@
 package main
 
+import (
+	Consensus "github.com/MikkelBKristensen/DSHandins/Handin5/Consensus"
+	_ "google.golang.org/grpc"
+)
+struct Client{
+	int64 lamportclock;
+	var stream Consensus_SyncClient;
+}
+
--- Auction/Server.go
+++ Auction/Server.go
@@ -1,2 +1,11 @@
 package main
+import (
+	Consensus "github.com/MikkelBKristensen/DSHandins/Handin5/Consensus"
+	_ "google.golang.org/grpc"
+)
 
+Server struct{
+	consensusServer Consensus.ConsensusServer
+	consensusClients []Consensus.ConsensusClient
+
+}
